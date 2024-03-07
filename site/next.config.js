d056f7abe029d77c76ee3126a4ec489b20ea28ee
JustOfPlay <126193393+JustOfPlay@users.noreply.github.com>
2024-03-07 22:11:29 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: 'https://user-images.githubusercontent.com/5698566/230345149-ef757e51-6eb9-479d-94f5-a13e4ad33b03.png',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://justofplay.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
